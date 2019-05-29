9e0a896b89ff159a60fe3077316c93d2ea288a2e
daniel <danielwil@wp.pl>
2019-05-29 14:57:19 +0200

    Updates

---
+++ precache-manifest.981cfaa4c0c07a91e0e3eed2971c7b3a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1fcbbdb65652f636921530c620be34",
+    "url": "/dkda-lzw/index.html"
+  },
+  {
+    "revision": "40fbc3754a6f2b41874b",
+    "url": "/dkda-lzw/static/css/2.e9f6a2f5.chunk.css"
+  },
+  {
+    "revision": "6e2c90f7e2e4be710aca",
+    "url": "/dkda-lzw/static/css/main.d3aa3591.chunk.css"
+  },
+  {
+    "revision": "40fbc3754a6f2b41874b",
+    "url": "/dkda-lzw/static/js/2.58355d69.chunk.js"
+  },
+  {
+    "revision": "6e2c90f7e2e4be710aca",
+    "url": "/dkda-lzw/static/js/main.3c7d0f1c.chunk.js"
+  },
+  {
+    "revision": "cb10017b4bcfeb600ba6",
+    "url": "/dkda-lzw/static/js/runtime~main.a84a0626.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/dkda-lzw/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/dkda-lzw/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/dkda-lzw/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/dkda-lzw/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/dkda-lzw/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
